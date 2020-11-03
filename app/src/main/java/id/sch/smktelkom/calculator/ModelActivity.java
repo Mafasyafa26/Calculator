a1b3b82b90767cc336db93140c4246bbfe6eb23b
MyViewAndViews <mafasyf2@gmail.com>
2020-11-03 10:41:16 +0700

    Initial commit

---
+++ app/src/main/java/id/sch/smktelkom/calculator/ModelActivity.java
@@ -0,0 +1,27 @@
+package id.sch.smktelkom.calculator;
+
+public class ModelActivity {
+    public double m_num1;
+    public double m_num2;
+
+    public ModelActivity(double num1, double num2) {
+        this.m_num1 = num1;
+        this.m_num2 = num2;
+    }
+
+    public void setNum1(double num1){
+        this.m_num1 = num1;
+    }
+
+    public void setNum2(double num2){
+        this.m_num2 = num2;
+    }
+
+    public double getNum1(){
+        return this.m_num1;
+    }
+
+    public double getNum2(){
+        return this.m_num2;
+    }
+}
